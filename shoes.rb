cb2f5b60a2bc5056b3a086938ab21c59c2923d02
Ellen Cooper <ellencooper74@gmail.com>
2017-05-17 21:19:14 -0600

    reformats shoes app

--- shoes.rb
+++ shoes.rb
@@ -5,14 +5,14 @@
  dictionary = File.read("/usr/share/dict/words")
  trie.populate(dictionary)
  
- Shoes.app :title => "CompleteMe" do 
-   background 'http://m.c.lnkd.licdn.com/mpr/mpr/p/4/005/054/08f/3a55e47.jpg'
-   flow do
+ Shoes.app :title => "CompleteMe", :width => 1000, :resizable => false do 
+   background './images/shoes_background.jpg'
+   flow :margin => [380, 100] do
      @edit_line = edit_line
-     button 'Submit' do 
+     button('Complete Me') do 
        @slot.clear if @slot
        @suggestions = trie.suggest(@edit_line.text)
-        @slot = flow do list_box items: @suggestions,
+        @slot = flow :margin => [380, 0] do list_box items: @suggestions,
           choose: @suggestions.first do |list|
             trie.select(@edit_line.text, list.text)
         end
